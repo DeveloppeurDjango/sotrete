106ca550051854fac02a7b2d54833531ac4c7591
DeveloppeurDjango <mranebaye@email.com>
2025-05-01 05:15:09 +0000

    statisfiles

--- sotreteapp/urls.py
+++ sotreteapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import home, about, service, contact, confirmation, search, detail, gallery
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name='home'),
@@ -16,3 +17,4 @@ urlpatterns = [
 
 # Servir les fichiers médias en dev et prod simple
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
