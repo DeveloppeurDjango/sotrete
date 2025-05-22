69b47b6ee35c17502db91d59f1b1d026a8116ff5
DeveloppeurDjango <mranebaye@email.com>
2025-05-22 14:14:26 +0000

    sitemap robots

--- sotreteapp/urls.py
+++ sotreteapp/urls.py
@@ -9,7 +9,7 @@ from django.views.static import serve
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
-    path('service/', service, name='service'),
+    path('services/', service, name='service'),
     path('gallery/', gallery, name='gallery'),
     path('contact/', contact, name='contact'),
     path('confirmation/', confirmation, name='confirmation'),
@@ -17,6 +17,7 @@ urlpatterns = [
     path('article/recherche/', search, name="search"),
     path('google439126b94a9dcf8a.html', serve, {'document_root': 'sotreteapp/templates', 'path': 'google439126b94a9dcf8a.html'}),
     path('sitemap.xml', serve, {'document_root': '.', 'path': 'sitemap.xml'}),
+    path('robots.txt', serve, {'document_root': '.', 'path': 'robots.txt'}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
